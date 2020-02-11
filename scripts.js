50428f0a9cdd7c81b088110c15c11370d4ed91d5
Jan <kankovskyj@email.cz>
2020-02-11 19:47:34 +0100

    navbar added, need a better overlay animation plus overlay shouldnt be 100%vh, so that you can see the scrolll effects when you click one of the hrefs. also hamburger animation fix needed

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+ //NAV BUTTON OPEN/CLOSE 
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+
 //FADE THE INDICATOR
 $(window).scroll(function(){
     if ($(this).scrollTop() > 50) {
@@ -5,4 +19,12 @@ $(window).scroll(function(){
     } else {
        //$('#fade-out').removeClass('dissapear');
     }
+});
+
+//OPEN MENU
+
+
+ //
+$('.menu-btn').on('click', function(){
+   $('.nav-bar').toggle();
 });
